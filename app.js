e630ca7fc76bff82ee784b844ec560f5c3b8e11b
anand-yadav55 <anand55yadav@gmail.com>
2020-12-01 18:35:27 +0530

    adding production port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('express') ();
 app.post('/getWeather',(req, res)=>{
-
+const PORT = process.env.PORT;
 });
 app.get('/', (req, res)=>{
     res.sendFile("./src/index.html", {root:__dirname});
@@ -11,6 +11,6 @@ app.get('/assets/style.css', (req, res)=>{
 app.get('/showWeather.js', (req, res)=>{
     res.sendFile('./src/showWeather.js', {root:__dirname});
 })
-app.listen("3000", ()=>{
+app.listen(PORT, ()=>{
     console.log("server running");
 });
